1d9f70e66e0ba739517847d8f77a6a49afcd4646
Mauricemannuel <mauricebaldain@gmail.com>
2016-11-30 16:44:49 -0800

    11/09/16

--- model/account_dal.js
+++ model/account_dal.js
@@ -5,19 +5,19 @@ var db  = require('./db_connection.js');
 var connection = mysql.createConnection(db.config);
 
 exports.getAll = function(callback) {
-    var query = 'SELECT * FROM school;';
+    var query = 'SELECT * FROM account;';
 
     connection.query(query, function(err, result) {
         callback(err, result);
     });
 };
 
-exports.getById = function(school_id, callback) {
-    var query = 'SELECT * FROM school WHERE school_id = ?';
-    var queryData = [school_id];
+exports.getById = function(account_id, callback) {
+    console.log(account_id)
+    var query = 'SELECT * FROM account WHERE account_id = ?';
+    var queryData = [account_id];
 
     connection.query(query, queryData, function(err, result) {
-        console.log('success')
         callback(err, result);
     });
 };
--- model/address_dal.js
+++ model/address_dal.js
@@ -5,16 +5,16 @@ var db  = require('./db_connection.js');
 var connection = mysql.createConnection(db.config);
 
 exports.getAll = function(callback) {
-    var query = 'SELECT * FROM school;';
+    var query = 'SELECT * FROM address;';
 
     connection.query(query, function(err, result) {
         callback(err, result);
     });
 };
 
-exports.getById = function(school_id, callback) {
-    var query = 'SELECT * FROM school WHERE school_id = ?';
-    var queryData = [school_id];
+exports.getById = function(address_id, callback) {
+    var query = 'SELECT * FROM address WHERE address_id = ?';
+    var queryData = [address_id];
 
     connection.query(query, queryData, function(err, result) {
         console.log('success')
